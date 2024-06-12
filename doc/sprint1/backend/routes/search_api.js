9798f4efd2ea85bbc7dcbdd58694fd4481f0978b
Eddy Chen <eddy.al.chen@gmail.com>
2024-06-12 00:29:32 -0400

    added a bit of framework regarding connecting search bar to neo4j (WIP)

--- doc/sprint1/backend/routes/search_api.js
+++ doc/sprint1/backend/routes/search_api.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+// connect to neo4j
+//const { driver, getSession } = require("../neo4j.js");
+
 router.post('/api/search', function (req, res) {
     console.log('Server received: POST /api/search');
     // An example of a search result
+    
+    //start the session
+    //const session = getSession();
+    //const resultList = [];
+    //session.run("Placeholder Text").then(result => {
+        //result.records.forEach(record => {
+            //store results of query into list
+            //resultList.push(record.get('placeholder name of node property'));
+        //});
+    //})
+    
     const user = {
         id: 1,
         username: 'john_doe',
