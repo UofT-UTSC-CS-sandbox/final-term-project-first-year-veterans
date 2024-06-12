759433a6156f41c6169ef41cabf0be5e85e7ff04
Ethan Rosenbloom <ethan.rosenbloom@mail.utoronto.ca>
2024-06-12 01:44:39 -0400

    added filters for Majors and Newest/Trending

--- doc/sprint1/backend/routes/search_api.js
+++ doc/sprint1/backend/routes/search_api.js
@@ -1,16 +1,99 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
 const router = express.Router();
 
-router.post('/api/search', function (req, res) {
-    console.log('Server received: POST /api/search');
-    // An example of a search result
-    const user = {
+// connect to neo4j
+//const { driver, getSession } = require("../neo4j.js");
+
+// An example of a search result
+
+//start the session
+//const session = getSession();
+//const resultList = [];
+//session.run("Placeholder Text").then(result => {
+    //result.records.forEach(record => {
+        //store results of query into list
+        //resultList.push(record.get('placeholder name of node property'));
+    //});
+//})
+
+const users = [
+    {
         id: 1,
-        username: 'john_doe',
-        email: 'john@example.com'
-    };
-    res.status(200);
-    res.json(user);
+        username: 'NotTrending1AndCrim',
+        email: 'john@example.com',
+        trending: false,
+        major: "criminology"
+    },
+    {
+        id: 2,
+        username: 'Trending1AndStats',
+        email: 'Trending1@example.com',
+        trending: true,
+        major: "statistics"
+    },
+    {
+        id: 3,
+        username: 'Trending2AndMath',
+        email: 'Trending2@example.com',
+        trending: true,
+        major: "math"
+    },
+    {
+        id: 4,
+        username: 'NotTrending2AndCS',
+        email: 'NotTrending@example.com',
+        trending: false, // Not trending
+        major: "computer_science"
+    },
+    {
+        id: 5,
+        username: 'Trending3AndCrim',
+        email: 'User5@example.com',
+        trending: true, // Not trending
+        major: "criminology"
+    }
+];
+
+router.post('/api/search/', function (req, res) {
+    console.log('Server received: POST /api/search/');
+    let search_data = req.body;
+    const state = search_data.filters;
+    console.log(search_data.query);
+
+    if (search_data.query == 'all'){
+        res.status(200);
+        return res.json(users);
+    }
+    else if (state.length != 0){
+        switch (state[0]){
+            case 'trending':
+                res.status(200); // Setting HTTP status code
+                const trendingUsers = users.filter(user => user.trending); // Filtering trending users
+                console.log(trendingUsers);
+                res.json(trendingUsers); // Sending JSON response with trending users
+                break;
+            case 'newest':
+                const newestUser = [users.reduce((max, user) => (user.id > max.id ? user : max), users[0])];
+                res.status(200);
+                console.log(newestUser);
+                res.json(newestUser);
+                break;
+            case 'math':
+            case 'computer_science':
+            case 'statistics':
+                res.status(200);
+                const filteredUsers = users.filter(user => user.major === state[0]);
+                console.log(filteredUsers);
+                res.json(filteredUsers);
+                break;
+        }
+    } else {
+        return res.json([users]);
+    }
+    
 });
 
 module.exports = router;
--- doc/sprint1/frontend/src/Components/search_bar.js
+++ doc/sprint1/frontend/src/Components/search_bar.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { useState } from 'react';
-import {api_search} from './api'; 
+import {api_search, api_filter_search} from './api'; 
+
+import Dropdown from 'react-bootstrap/Dropdown';
 import '../Asset/Css/App.css';
 
 function SearchBar() {
     const [query, setQuery] = useState('');
+    const [filters, setFilters] = useState([]);
     const [results, setResults] = useState([]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
-        const search_data = { query };
-
+        const search_data = {"query": query, "filters":filters}
         const callback = (data) => {
-            setResults([data]);
+            setResults(data);
         };
 
         api_search(search_data, callback);
     };
+    
+    // General handle filter function
+    const handleFilter = (event, filter_type) => {
+        event.preventDefault();
+        const search_data = {"query": query, "filters":[filter_type]}
+        const callback = (data) => {
+            setFilters(filter_type)
+            setResults(data);
+        };
+        api_search(search_data, callback);
+    }
 
     return (
         <div>
@@ -31,11 +43,33 @@ function SearchBar() {
                     value={query}
                     onChange={(e) => setQuery(e.target.value)}
                 ></input>
-                <button type="submit">Search</button>
+                <button className = "submitbutton" type="submit">Search</button>
             </form>
+            <div className = "dropdown-container">
+                <Dropdown>
+                    <Dropdown.Toggle className="dropbtn" variant="success" id="dropdown-basic">
+                        Filters
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item className="dropdown-content" onClick={(e) => handleFilter(e, 'newest')}>Newest</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-content" onClick={(e) => handleFilter(e, 'trending')}>Trending</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle className="dropbtn" variant="success" id="dropdown-basic">
+                        Majors
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item className="dropdown-content" onClick={(e) => handleFilter(e, 'computer_science')}>Computer Science</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-content" onClick={(e) => handleFilter(e, 'math')}>Math</Dropdown.Item>
+                        <Dropdown.Item className="dropdown-content" onClick={(e) => handleFilter(e, 'statistics')}>Statistics</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
             <div>
                 <h2>Search Results:</h2>
                 <ul>
+                {console.log(results)}
                 {results.map((result, index) => (
                     <li key={index}>{result.username} ({result.email})</li>
                 ))}
