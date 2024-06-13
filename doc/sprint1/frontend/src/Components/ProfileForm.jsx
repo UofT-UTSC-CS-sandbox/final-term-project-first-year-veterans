b71b6414ed1fb920feaad78d53cb822d0d4d679f
ChiehAnChang <chiehan.chang@mail.utoronto.ca>
2024-06-13 10:25:10 -0400

    Fix the structure of profile

--- doc/sprint1/frontend/src/Components/ProfileForm.jsx
+++ doc/sprint1/frontend/src/Components/ProfileForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../Asset/Css/profilePicture.css";
 
 function ProfileForm() {
     const [modify, setModify] = useState(false);
@@ -51,54 +52,39 @@ function ProfileForm() {
       <form className="container">
             <h1 className="row justify-content-center">Profile</h1>
             <h2 className="row justify-content-center">Hi, {firstName} {lastName}</h2>
-            <div className="column "><img src="../public/avatarPlaceHolder.png" alt="" /></div>
-            <div className="column ">
-                <div className="row justify-content-end">
-                <div className="col-md-6 mb-3">
-                    <label className="mr-3" htmlFor="firstName">First name: </label>
-                    {modify?<input type="text" id="firstName" placeholder="Harry" onChange={onChange} value={firstName}/>:<input type="text" id="firstName" placeholder="Harry"   readOnly/>}
-                </div>
-                </div>
-                <div className="row justify-content-end">
-                <div className="col-md-6 mb-3">
-                    <label htmlFor="lastName">Last name: </label>
-                    {modify?<input type="text" id="lastName" placeholder="Potter"  onChange={onChange} value={lastName}  />:<input type="text" id="lastName" placeholder="Potter"   readOnly/>}
-                </div>
-                </div>
-                <div className="row justify-content-end">
-                <div className="col-md-6 mb-3">
-                    <label htmlFor="Email">Email: </label>
-                    {modify?<input type="email" id="Email" placeholder="abc@mail.com" onChange={onChange} value={email}  />: <input type="text" id="htmlhtmlFor" placeholder="abc@mail.com"   readOnly/>}
-                </div>
-                </div>
-                <div className="row justify-content-end">
-                <div className="col-md-6 mb-3">
-                    <label htmlFor="Phone">Phone: </label>
-                    {modify?<input type="tel" id="Phone" placeholder="(123)-456-7890" onChange={onChange} value={phone}  />:<input type="text" id="Phone" placeholder="(123)-456-7890"   readOnly/>}
-                </div>
-                </div>
-                <div className="row justify-content-end">
-                <div className="col-md-6 mb-3">
-                    <label htmlFor="University">University: </label>
-                    {modify?<input type="text" id="University" placeholder="Hogwarts" onChange={onChange} value={university}  />:<input type="text" id="University" placeholder="Hogwarts"   readOnly/>}
-                </div>
-                </div>
-                <div className="row justify-content-end">
-                <div className="col-md-6 mb-3">
-                    <label htmlFor="Major">Major: </label>
-                    { modify?<input type="text" id="Major" placeholder="Magic" onChange={onChange} value={major}  />:<input type="text" id="Major" placeholder="Magic"   readOnly/>}
-                </div>
-                </div>
-                <div className="row justify-content-end">
-                    <div className="col-md-6 mb-3">
-                        <label htmlFor="Habit">Habit: </label>
-                        {modify?<input type="text" id="Habit" placeholder="Quidditch"  onChange={onChange} value={habit} />:<input type="text" id="Habit" placeholder="Quidditch"   readOnly/>}
-                    </div>
-                </div>
-                <div className="row justify-content-end">
-                    <button  onClick={handleModify}>{!modify?"Modified":"Confirm"}</button>
-                </div>
+            <div className="row justify-content-end">
+              <div className="col ">
+                <img src="../../public/avatarPlaceHolder.png" alt="" className="rounded-circle avatar" />
+              </div>
+              <div className="col-md-6 mb-3">
+                <label className="mr-3" htmlFor="firstName">First name: </label>
+                {modify?<input type="text" id="firstName" placeholder="Harry" onChange={onChange} value={firstName}/>:<input type="text" id="firstName" placeholder="Harry"   readOnly/>}
+                <br/>
+                <label htmlFor="lastName">Last name: </label>
+                {modify?<input type="text" id="lastName" placeholder="Potter"  onChange={onChange} value={lastName}  />:<input type="text" id="lastName" placeholder="Potter"   readOnly/>}
+                <br/>
+                <label htmlFor="Email">Email: </label>
+                {modify?<input type="email" id="Email" placeholder="abc@mail.com" onChange={onChange} value={email}  />: <input type="text" id="htmlhtmlFor" placeholder="abc@mail.com"   readOnly/>}
+                <br/>
+                <label htmlFor="Phone">Phone: </label>
+                {modify?<input type="tel" id="Phone" placeholder="(123)-456-7890" onChange={onChange} value={phone}  />:<input type="text" id="Phone" placeholder="(123)-456-7890"   readOnly/>}
+                <br/>
+                <label htmlFor="University">University: </label>
+                {modify?<input type="text" id="University" placeholder="Hogwarts" onChange={onChange} value={university}  />:<input type="text" id="University" placeholder="Hogwarts"   readOnly/>}
+                <br/>
+                <label htmlFor="Major">Major: </label>
+                { modify?<input type="text" id="Major" placeholder="Magic" onChange={onChange} value={major}  />:<input type="text" id="Major" placeholder="Magic"   readOnly/>}
+                <br/>
+                <label htmlFor="Habit">Habit: </label>
+                {modify?<input type="text" id="Habit" placeholder="Quidditch"  onChange={onChange} value={habit} />:<input type="text" id="Habit" placeholder="Quidditch"   readOnly/>}
+
+              </div>
             </div>
+                    
+            <div >
+                <button  onClick={handleModify}>{!modify?"Modified":"Confirm"}</button>
+            </div>
+
           </form>
     );
 }
