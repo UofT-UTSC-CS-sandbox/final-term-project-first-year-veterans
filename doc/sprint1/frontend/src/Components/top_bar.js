9be77a2a269ff8630827a0e0c888302099006386
Ethan Rosenbloom <ethan.rosenbloom@mail.utoronto.ca>
2024-06-15 18:20:23 -0400

    added way back home from profile page

--- doc/sprint1/frontend/src/Components/top_bar.js
+++ doc/sprint1/frontend/src/Components/top_bar.js
@@ -21,7 +21,7 @@ function TopBar() {
           </Navbar.Brand>
 
           <ul className="nav nav-pills">
-            <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+            <li className="nav-item"><a href="#" className="nav-link active" aria-current="page" onClick={(e) => handleClick(e, 'Search')}>Home</a></li>
             <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
             <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
             <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
