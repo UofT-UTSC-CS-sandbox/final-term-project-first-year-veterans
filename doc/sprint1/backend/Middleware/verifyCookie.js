3a1d1e7317edd7d8dcec29f63f7530620ef9134e
ChiehAnChang <90326818+ChiehAnChang@users.noreply.github.com>
2024-07-02 10:20:43 -0400

    Add a guildline to both frontend and backend developer

--- doc/sprint1/backend/Middleware/verifyCookie.js
+++ doc/sprint1/backend/Middleware/verifyCookie.js
@@ -3,22 +3,54 @@
 */
 const { verify } = require('jsonwebtoken');
 const { SECRET_KEY } = require('./seceretKey');
+const { TokenExpiredError } = require('jsonwebtoken');
+
+
+const generalErrorMsg = "Failed to authenticate token"
+
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.auth;   // This code will provide the authentication token from the cookie
+  /*
 
+    How to use:
+
+      Frontend Developer: Should always check if the status code is 302 and redirectToLogin is true. 
+                          If it is true, then redirect the user to the login page using navigation hook.
+                      
+      Backend Developer: Use this middleware in the routes where you want to check if the user is authenticated or not.
+                        For example, in the calendar_api.js, I have used this middleware to check if the user is authenticated or not to get the events
+                        by calling this function inside router. i.e:router.get('/api/events',verifyToken , function(req, res) {some code here});
+                        
+                        Warning: If the exist token is not exist or expired or any other error, the remaining code inside the router will not be executed.
+
+    Description:
+      This function will be used when the user has been login and with token is not expired.
+      If the token has been expired, we should logout the user and let the user login again.
+
+  */
+
+  const token = req.cookies.auth;   // This code will provide the authentication token from the cookie
+  
   
   if (!token) {
-    return res.status(403).send('No token provided');
+
+    // This means that token never existed; which is strange because the user should have been authenticated before accessing this route.
+    return res.status(302).json({ redirectToLogin: true , msgErr: "You should have been authenticated before accessing this route, but you don't have a token ?!" });
+  
   }
 
   verify(token, SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(500).send('Failed to authenticate token');
-    }
 
-    req.userId = decoded.id;
-    next();
+      // Since the error is invalid token, we should logout the user and let the user login again. We should also clear the cookie.
+
+      res.clearCookie('auth', { httpOnly: true, secure: process.env.NODE_ENV === 'production' }); 
+      return res.status(302).json({ redirectToLogin: true , msgErr: generalErrorMsg });      
+      
+    }else{
+      req.userId = decoded.id;
+      next();
+    }
   });
 };
 
