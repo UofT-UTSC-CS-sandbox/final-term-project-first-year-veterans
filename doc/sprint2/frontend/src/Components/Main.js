7ddf14f3e9a716ab6d41aaeda398559308d3a38a
RichieHsieh <richie.hsieh@mail.utoronto.ca>
2024-07-07 02:55:39 -0400

    add sprint2

---
+++ doc/sprint2/frontend/src/Components/Main.js
@@ -0,0 +1,41 @@
+import logo from '../logo.svg';
+import React from 'react';
+import TopBar from './top_bar';
+import SearchBar from './search_bar';
+import ProfileForm from './ProfileForm';
+import FriendList from './FriendList';
+import UserPage from './UserPage';
+import { PageProvider, usePage } from './PageContext';
+import { useState } from 'react';
+
+
+function Main() {
+  return (
+    <PageProvider>
+      <div className="Main">
+          <TopBar />
+          <PageContent />
+      </div>
+  </PageProvider>
+  );
+}
+
+const PageContent = () => {
+  const { currentPage } = usePage();
+  const [userpageInfo, setUserpageInfo] = useState('');
+
+  if (currentPage === 'Search') {
+      return <SearchBar />;
+  } else if (currentPage === 'Profile') {
+      return <ProfileForm />;
+  } else if (currentPage === 'Friends') {
+      return <FriendList setUserpageInfo={setUserpageInfo}/>;
+  } else if (currentPage === 'User') {
+      console.log("userInfo: ", userpageInfo);
+      return <UserPage userpageInfo={userpageInfo}/>;
+  } else {
+      return null;
+  }
+};
+
+export default Main;
---
+++ doc/sprint2/frontend/src/Components/top_bar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Asset/Css/App.css';
+import { usePage } from './PageContext';
+import UserTools from './UserTools';
+
+function TopBar() {
+  const { currentPage, handlePageChange } = usePage();
+
+  const handleClick = (e, page) => {
+    e.preventDefault();
+    handlePageChange(page);
+  };
+  
+  return (
+    <div className="Top_Bar">
+      <div className="container">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <Navbar.Brand href="/" className="d-flex align-items-center me-md-auto link-body-emphasis"> 
+            <span className="webName fs-4">EDU PRODIGI</span>
+          </Navbar.Brand>
+
+          <ul className="nav nav-pills">
+            <li className="nav-item"><a href="#" className={currentPage === 'Search' ? "nav-link active" : "nav-link"} aria-current="page" onClick={(e) => handleClick(e, 'Search')}>Home</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+            <li className="nav-item"><UserTools handleClick={handleClick}/></li>
+          </ul>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
