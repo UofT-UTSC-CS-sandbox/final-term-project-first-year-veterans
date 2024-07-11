6994cfa4cb4d52bd17c17a2a6d7e8a72877d6fd5
ChiehAnChang <chiehan.chang@mail.utoronto.ca>
2024-07-11 18:09:43 -0400

    delete all sprint 2

--- doc/sprint2/backend/helper_functions/auth.js
+++
@@ -1,9 +0,0 @@
-function checkAuth(req) {
-    const token = req.cookies['cookie_name'];
-    if (token && token === 'cookie_value') {
-        return true;
-    }
-    return false;
-}
-
-module.exports = { checkAuth };
